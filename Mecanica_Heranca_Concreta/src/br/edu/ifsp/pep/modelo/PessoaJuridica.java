a112090c7c9c72a6a05577617d5f989effbd00bb
Joao Pedro <francasjoaopedro@gmail.com>
2024-09-07 04:17:47 -0300

    Tabelas Concretas
    Entendendo o funcionamento de tabelas concretas no JPA

---
+++ Mecanica_Heranca_Concreta/src/br/edu/ifsp/pep/modelo/PessoaJuridica.java
@@ -0,0 +1,28 @@
+package br.edu.ifsp.pep.modelo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pessoa_juridica")
+public class PessoaJuridica extends Pessoa {
+    
+    @Column(name = "cnpj", length = 18, nullable = false)
+    private String cnpj;
+
+    public PessoaJuridica() {
+    }
+
+    public PessoaJuridica(String cnpj) {
+        this.cnpj = cnpj;
+    }
+    
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+}
